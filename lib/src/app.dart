d54a2aed7a316d641336d0c71e1d2b63e1a04675
Evandro Santos <egs.evandro.santos@gmail.com>
2019-09-24 15:53:26 -0300

    Remove unused code and architecture BLoC pattern

---
+++ lib/src/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Maps and Geolocation',
+      debugShowCheckedModeBanner: false,
+      home: Container(
+        color: Colors.white,
+      ),
+    );
+  }
+}
